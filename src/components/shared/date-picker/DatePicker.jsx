a657325eaf104a2baa613643352ab656a6dac012
abbygoyal <abhishekgoyal3669@gmail.com>
2023-06-16 16:22:37 +0530

    fix(task):weekend highlight removed

--- src/components/shared/date-picker/DatePicker.jsx
+++ src/components/shared/date-picker/DatePicker.jsx
@@ -1,3 +1,73 @@
+import { useState } from "react";
+import DatePicker from "@hassanmojab/react-modern-calendar-datepicker";
+import "@hassanmojab/react-modern-calendar-datepicker/lib/DatePicker.css";
+
+export default function DatePickerComponent(props) {
+  const [date, setDate] = useState();
+
+  const renderCustomInput = ({ ref }) => {
+    const formattedDate = date
+      ? new Date(date.year, date.month - 1, date.day).toLocaleDateString(
+          "en-US",
+          {
+            weekday: "short",
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+          }
+        )
+      : "";
+
+    return (
+      <div>
+        <input
+          readOnly
+          placeholder={"Choose date"}
+          style={{ width: "100%" }}
+          ref={ref}
+          value={formattedDate}
+          className="btn btn-gray btn-w-350"
+        />
+      </div>
+    );
+  };
+
+  const onChange = (dateValue) => {
+    if (dateValue !== null && dateValue !== undefined) {
+      setDate(dateValue);
+      props.onChange &&
+        props.onChange(
+          new Date(dateValue.year, dateValue.month - 1, dateValue.day)
+        );
+      console.log(dateValue); // Log selected date to the console
+    }
+  };
+
+  // let nowDate = new Date(Date.now());
+  // let minDate = {
+  //   year: nowDate.getFullYear(),
+  //   month: nowDate.getMonth() + 1,
+  //   day: nowDate.getDate(),
+  // };
+
+  return (
+    <div>
+      <DatePicker
+        calendarPopperPosition="bottom"
+        // shouldHighlightWeekends
+        inputClassName="form-control"
+        value={date}
+        // minimumDate={minDate}
+        onChange={(dateValue) => onChange(dateValue)}
+        colorPrimary="#42ade2"
+        renderInput={renderCustomInput}
+      />
+    </div>
+  );
+}
+
+// old code //
+
 // import { useState } from "react";
 // import DatePicker from "@hassanmojab/react-modern-calendar-datepicker";
 // // import CalenderIcon from '../../../assets/images/Calender'
@@ -64,71 +134,3 @@
 //     </div>
 //   );
 // }
-
-import { useState } from "react";
-import DatePicker from "@hassanmojab/react-modern-calendar-datepicker";
-import "@hassanmojab/react-modern-calendar-datepicker/lib/DatePicker.css";
-
-export default function DatePickerComponent(props) {
-  const [date, setDate] = useState();
-
-  const renderCustomInput = ({ ref }) => {
-    const formattedDate = date
-      ? new Date(date.year, date.month - 1, date.day).toLocaleDateString(
-          "en-US",
-          {
-            weekday: "short",
-            day: "2-digit",
-            month: "short",
-            year: "numeric",
-          }
-        )
-      : "";
-
-    return (
-      <div>
-        <input
-          readOnly
-          placeholder={"Choose date"}
-          style={{ width: "100%" }}
-          ref={ref}
-          value={formattedDate}
-          className="btn btn-gray btn-w-350"
-        />
-      </div>
-    );
-  };
-
-  const onChange = (dateValue) => {
-    if (dateValue !== null && dateValue !== undefined) {
-      setDate(dateValue);
-      props.onChange &&
-        props.onChange(
-          new Date(dateValue.year, dateValue.month - 1, dateValue.day)
-        );
-      console.log(dateValue); // Log selected date to the console
-    }
-  };
-
-  // let nowDate = new Date(Date.now());
-  // let minDate = {
-  //   year: nowDate.getFullYear(),
-  //   month: nowDate.getMonth() + 1,
-  //   day: nowDate.getDate(),
-  // };
-
-  return (
-    <div>
-      <DatePicker
-        calendarPopperPosition="bottom"
-        shouldHighlightWeekends
-        inputClassName="form-control"
-        value={date}
-        // minimumDate={minDate}
-        onChange={(dateValue) => onChange(dateValue)}
-        colorPrimary="#42ade2"
-        renderInput={renderCustomInput}
-      />
-    </div>
-  );
-}
