ab3b14b2b96e08d655c739e97f29f293239ad5b9
topdev7 <ednoguer00@gmail.com>
2023-06-18 10:45:03 +0300

    update minor issues

--- src/components/Login/LoginSteps/AboutStep.jsx
+++ src/components/Login/LoginSteps/AboutStep.jsx
@@ -1,37 +1,30 @@
 import { useState } from "react";
 import LocationSelection from "../../NewTask/LocationSelection/LocationSelection";
 import { editProfile } from "../../../redux/actions";
-import { connect } from 'react-redux';
-
+import { connect } from "react-redux";
 
 function AboutStep(props) {
-
   const [locationSelectionModal, setLocationSelectionModal] = useState(false);
   const [address, setAddress] = useState();
-  const [latlng, setLatlng] = useState({lat:0, lng:0})
+  const [latlng, setLatlng] = useState({ lat: 0, lng: 0 });
   const [fullName, setFullName] = useState({
-    firstName:null,
-    lastName:null
+    firstName: null,
+    lastName: null,
   });
 
   const isValidForm = () => {
-    if (
-      fullName.firstName &&
-      fullName.lastName
-    )
-      return true;
+    if (fullName.firstName && fullName.lastName) return true;
     return false;
   };
 
   const onContinue = () => {
-    if(isValidForm()) {
-      props.editProfile({fullName:fullName, address:latlng})
-      props.onContinue()
+    if (isValidForm()) {
+      props.editProfile({ fullName: fullName, address: latlng });
+      props.onContinue();
     } else {
-      console.log("error==================>")
+      console.log("error==================>");
     }
-  }
-
+  };
 
   return (
     <>
@@ -57,24 +50,37 @@ function AboutStep(props) {
           </p>
           <div className="form-control-group mt-20">
             <p className="font-bold mb-10">First name</p>
-            <input className="w-100 px-2" onChange={(e) => setFullName({...fullName, firstName:e.target.value})} />
+            <input
+              className="w-100 px-2"
+              onChange={(e) =>
+                setFullName({ ...fullName, firstName: e.target.value })
+              }
+            />
           </div>
           <div className="form-control-group mt-20">
             <p className="font-bold mb-10">Last name</p>
-            <input className="w-100 px-2" onChange={(e) => setFullName({...fullName, lastName:e.target.value})}/>
+            <input
+              className="w-100 px-2"
+              onChange={(e) =>
+                setFullName({ ...fullName, lastName: e.target.value })
+              }
+            />
           </div>
           <div className="mt-20">
             <p className="font-bold mb-10">Location</p>
-            <button className="d-block btn btn-gray btn-w-350"
+            <button
+              className="d-block btn btn-gray btn-w-350"
               onClick={() => setLocationSelectionModal(true)}
             >
-              <div style={{display:"flex", justifyContent:"center"}}>
+              <div className="d-flex justify-content-center align-items-center">
                 <img
                   src="./assets/images/icons/location-dark.svg"
                   alt="close"
                   onClick={props.onClose}
                 />
-                <p className="font-bold ml-10">{address ? address:"Choose Location"}</p>
+                <p className="font-bold ml-10">
+                  {address ? address : "Choose Location"}
+                </p>
               </div>
             </button>
           </div>
@@ -99,13 +105,13 @@ function AboutStep(props) {
   );
 }
 
-const mapStateToProps = ({ userReducer}) => {
-  const {message, error} = userReducer;
-  return {message, error};
+const mapStateToProps = ({ userReducer }) => {
+  const { message, error } = userReducer;
+  return { message, error };
 };
 
 const mapDispatchToProps = {
   editProfile,
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AboutStep);
--- src/components/shared/uploader/Uploader.jsx
+++ src/components/shared/uploader/Uploader.jsx
@@ -1,14 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Uploader.scss";
 
-export default class UploaderComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { images: [] };
-  }
+export default function UploaderComponent({ renderBtn }) {
+  const [images, setImages] = useState([]);
 
-  handleChange = (event) => {
-    let images_url = this.state.images ? [...this.state.images] : [];
+  const handleChange = (event) => {
+    let images_url = images ? [...images] : [];
     Object.entries(event.target.files).forEach(async (element) => {
       let file = element[1];
       if (
@@ -18,78 +15,70 @@ export default class UploaderComponent extends React.Component {
       )
         return;
       images_url.push({ path: URL.createObjectURL(file) });
-      this.setState({ images: images_url });
+      setImages(images_url);
     });
   };
 
-  remove = (index) => {
-    let tempImages = [...this.state.images];
+  const remove = (index) => {
+    let tempImages = [...images];
     tempImages.splice(index, 1);
 
-    this.setState({ images: tempImages });
+    setImages(tempImages);
     this.forceUpdate();
   };
 
-  clear = () => {
-    this.setState({ images: [] });
+  const clear = () => {
+    setImages([]);
   };
 
-  render() {
-    const { images } = this.state;
-
-    return (
-      <div className="uploader">
-        <div>
-          <div>
-            {images && images.length > 0 && (
-              <div className="attachment-task">
-                {images.map((image, key) => {
-                  return (
-                    <div className="area-img pt-md" key={key}>
-                      <img
-                        style={{ width: "100%", height: "100%" }}
-                        src={image.path}
-                        alt=""
-                      />
-                      <button className="close position-absolute bg-transparent border-0">
-                        <img
-                          src="./assets/images/icons/close.svg"
-                          alt="close"
-                          onClick={() => this.remove(key)}
-                        />
-                      </button>
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-          <div>
-            <div className="area">
-              {this.props.renderBtn ? (
-                this.props.renderBtn()
-              ) : (
-                <button className="d-block btn btn-gray btn-w-350 mt-3">
+  return (
+    <>
+      <div className="position-relative">
+        {images && images.length > 0 && (
+          <div className="attachment-task">
+            {images.map((image, key) => {
+              return (
+                <div className="area-img pt-md" key={key}>
                   <img
-                    src="./assets/images/icons/attachment.svg"
-                    alt="close"
-                    className="mr-10"
+                    style={{ width: "100%", height: "100%" }}
+                    src={image.path}
+                    alt=""
                   />
-                  Add attachment
-                </button>
-              )}
-              <input
-                id="file-upload"
-                onChange={this.handleChange}
-                className="input file-upload-input"
-                type="file"
-                accept="image/*"
-                multiple
-              />
-            </div>
+                  <button className="close position-absolute bg-transparent border-0">
+                    <img
+                      src="./assets/images/icons/close.svg"
+                      alt="close"
+                      onClick={() => remove(key)}
+                    />
+                  </button>
+                </div>
+              );
+            })}
           </div>
-        </div>
+        )}
       </div>
-    );
-  }
+      <div className="uploader">
+        {renderBtn ? (
+          renderBtn()
+        ) : (
+          <button className="d-block btn btn-gray btn-w-350 mt-3">
+            <img
+              src="./assets/images/icons/attachment.svg"
+              alt="close"
+              className="mr-10"
+            />
+            Add attachment
+          </button>
+        )}
+        <input
+          id="file-upload"
+          onChange={(e) => handleChange(e)}
+          className="input file-upload-input"
+          type="file"
+          accept="image/*"
+          multiple
+        />
+      </div>
+    </>
+  );
 }
