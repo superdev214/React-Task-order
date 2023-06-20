5b3aaf3c1aadb583ce93b74464b29429c335eb0c
milangdev <milanghevariyadev@gmail.com>
2023-06-20 18:41:29 +0530

    fix:home page animation

--- src/components/Home/HomeFooter.jsx
+++ src/components/Home/HomeFooter.jsx
@@ -1,6 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { debounce } from 'lodash';
+
 export default function HomeFooter() {
+  const [showFullImage, setShowFullImage] = useState(false);
+
+  useEffect(() => {
+    let bottomValue;
+    let isScrolling =true;
+    const handleTouchEnd = () => {
+      isScrolling = false;
+      setShowFullImage(false);
+    };
+    const trackScrolling = () => {
+      const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight) {
+        console.log("true");
+        bottomValue = scrollTop;
+        setShowFullImage(true);
+      } else {
+        if (showFullImage === true && isScrolling === false) {
+          console.log("false");
+          setShowFullImage(false);
+        }
+      }
+    };
+    document.addEventListener('touchend', handleTouchEnd);
+    window.addEventListener('scroll', trackScrolling);
+    return () => {
+      window.removeEventListener('scroll', trackScrolling);
+    };
+  }, [showFullImage]);
+
   return (
-    <section className="home-bottom" style={{ paddingBottom: "150px" }}>
+    <section className={`home-bottom`} style={{ paddingBottom: "150px" }}>
       <div className="container">
         <p className="home-bottom__txt">
           Get your
@@ -8,12 +40,14 @@ export default function HomeFooter() {
           tasks done.
         </p>
       </div>
-      <img
-        className="home-bottom__img"
-        style={{ paddingTop: "20px", paddingBottom: "20px" }}
-        src="./assets/images/earth.png"
-        alt="earth"
-      />
+      <div id='imagediv' className={`${showFullImage ? 'show-full' : 'show-half'}`}>
+        <img
+          className="home-bottom__img image-container-img"
+          style={{ paddingTop: "20px", paddingBottom: "20px" }}
+          src="./assets/images/earth.png"
+          alt="earth"
+        />
+      </div>
     </section>
   );
 }
