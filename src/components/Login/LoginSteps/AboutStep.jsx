a97c79e5c6a62a2bbf5ab5d41cd1cc87433cdf23
topdev7 <ednoguer00@gmail.com>
2023-06-21 13:41:09 +0300

    fix class name

--- src/components/Login/LoginSteps/AboutStep.jsx
+++ src/components/Login/LoginSteps/AboutStep.jsx
@@ -16,8 +16,8 @@ function AboutStep(props) {
   const [showUnauthorized, setShowUnauthorized] = useState(false);
   const token = useSelector((state) => state.userReducer?.token);
 
-  if(token) {
-    localStorage.setItem('token', token);
+  if (token) {
+    localStorage.setItem("token", token);
   }
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -56,82 +56,82 @@ function AboutStep(props) {
 
   return (
     <>
-    {token ? <section id="verify-code">
-        <div style={{ padding: "13px 0" }}>
-          <div className="container">
-            <div className="d-flex align-items-center justify-content-center">
-              <button className="position-absolute bg-transparent border-0 close-btn">
-                <img
-                  src="./assets/images/icons/close.svg"
-                  alt="close"
-                  onClick={props.onClose}
-                />
-              </button>
-              <p className="nav-title">About</p>
+      {token ? (
+        <section id="verify-code">
+          <div className="top-bar d-flex align-items-center">
+            <div className="container">
+              <div className="d-flex align-items-center justify-content-center">
+                <button className="position-absolute bg-transparent border-0 close-btn">
+                  <img
+                    src="./assets/images/icons/close.svg"
+                    alt="close"
+                    onClick={props.onClose}
+                  />
+                </button>
+                <p className="nav-title">About</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="container text-initial mt-20">
-          <p className="about-heading">
-            Tell us about yourself to setup your profile
-          </p>
-          <div className="form-control-group mt-20">
-            <p className="font-bold mb-10">First name</p>
-            <input
-              className="w-100 px-2"
-              onChange={(e) =>
-                setFullName({ ...fullName, firstName: e.target.value })
-              }
-            />
+          <div className="container text-initial mt-20">
+            <p className="about-heading">
+              Tell us about yourself to setup your profile
+            </p>
+            <div className="form-control-group mt-20">
+              <p className="font-bold mb-10">First name</p>
+              <input
+                className="w-100 px-2"
+                onChange={(e) =>
+                  setFullName({ ...fullName, firstName: e.target.value })
+                }
+              />
+            </div>
+            <div className="form-control-group mt-20">
+              <p className="font-bold mb-10">Last name</p>
+              <input
+                className="w-100 px-2"
+                onChange={(e) =>
+                  setFullName({ ...fullName, lastName: e.target.value })
+                }
+              />
+            </div>
+            <div className="mt-20">
+              <p className="font-bold mb-10">Location</p>
+              <button
+                className="d-block btn btn-gray btn-w-350"
+                onClick={() => setLocationSelectionModal(true)}
+              >
+                <div className="d-flex justify-content-center align-items-center">
+                  <img
+                    src="./assets/images/icons/location-dark.svg"
+                    alt="close"
+                    onClick={props.onClose}
+                  />
+                  <p className="font-bold ml-10">
+                    {address ? address : "Choose Location"}
+                  </p>
+                </div>
+              </button>
+            </div>
+            {locationSelectionModal && (
+              <LocationSelection
+                onChange={(address) => setAddress(address)}
+                onGetValue={(latlng) => setLatlng(latlng)}
+                close={() => setLocationSelectionModal(false)}
+              />
+            )}
           </div>
-          <div className="form-control-group mt-20">
-            <p className="font-bold mb-10">Last name</p>
-            <input
-              className="w-100 px-2"
-              onChange={(e) =>
-                setFullName({ ...fullName, lastName: e.target.value })
-              }
-            />
-          </div>
-          <div className="mt-20">
-            <p className="font-bold mb-10">Location</p>
+          <div className="fixed-bottom">
             <button
-              className="d-block btn btn-gray btn-w-350"
-              onClick={() => setLocationSelectionModal(true)}
+              className="d-block btn btn-green btn-w-350"
+              onClick={onContinue}
             >
-              <div className="d-flex justify-content-center align-items-center">
-                <img
-                  src="./assets/images/icons/location-dark.svg"
-                  alt="close"
-                  onClick={props.onClose}
-                />
-                <p className="font-bold ml-10">
-                  {address ? address : "Choose Location"}
-                </p>
-              </div>
+              Continue
             </button>
           </div>
-          {locationSelectionModal && (
-            <LocationSelection
-              onChange={(address) => setAddress(address)}
-              onGetValue={(latlng) => setLatlng(latlng)}
-              close={() => setLocationSelectionModal(false)}
-            />
-          )}
-        </div>
-        <div className="fixed-bottom">
-          <button
-            className="d-block btn btn-green btn-w-350"
-            onClick={onContinue}
-          >
-            Continue
-          </button>
-        </div>
-      </section>
-      
-    : showUnauthorized ? (
-      <div>Unauthorized</div>
-    ) : null}
+        </section>
+      ) : showUnauthorized ? (
+        <div>Unauthorized</div>
+      ) : null}
     </>
   );
 }
