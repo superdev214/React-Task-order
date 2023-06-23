20605de35755d346c8c0f72ac9b87bb4e2405ad0
milangdev <milanghevariyadev@gmail.com>
2023-06-23 16:36:42 +0530

    fix: home page amimation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,27 +6,22 @@ import "./Home.scss";
 
 export default function Home() {
   const divRef = useRef(null);
-  const [showDiv, setShowDiv] = useState(false);
-  const [showFullImage, setShowFullImage] = useState(false);
   const [touchedEvent, setTouchedEvent] = useState(true);
 
   const handleScroll = () => {
     const { scrollTop, scrollHeight, clientHeight } = divRef.current;
-    if (scrollTop < 0) {
-      setShowDiv(true);
+    if (scrollTop < 0 && !touchedEvent) {
+      divRef.current.scrollTo({ top: 140, behavior: "instant" });
+      setTouchedEvent(true)
     }
-    if (scrollTop > -10) {
-      setShowDiv(false);
+    if (scrollTop < 140 && !touchedEvent) {
+      divRef.current.scrollTo({ top: 140, behavior: 'auto' });
+      setTouchedEvent(true)
     }
-    if (scrollTop + clientHeight >= scrollHeight && touchedEvent) {
-      if (scrollTop + clientHeight >= scrollHeight + 70) {
-        setShowFullImage(true);
-      }
-    } else {
-      setShowFullImage(false)
-    }
-    if (scrollTop + clientHeight < scrollHeight + 70 && touchedEvent === false) {
-      setShowFullImage(false)
+    if (scrollTop + clientHeight > scrollHeight - 70 && touchedEvent === false) {
+      const img = document.getElementById('imagediv');
+      const scrollPosition = (img.scrollHeight / 2) - 70;
+      divRef.current.scrollTo({ top: scrollPosition });
       setTouchedEvent(true);
     }
   };
@@ -47,9 +42,9 @@ export default function Home() {
 
   return (
     <main ref={divRef} id="main_wrapper" onScroll={handleScroll} onTouchEnd={trackScrolling}>
-      <Slogan showDiv={showDiv} />
+      <Slogan />
       <TaskCategories />
-      <HomeFooter showFullImage={showFullImage} />
+      <HomeFooter />
     </main>
   );
 }
